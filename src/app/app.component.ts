901a33fde35db2f6a351b1d02303fedd15ca1c6a
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-15 15:27:14 +0530

    44. Adding More Components to the Demo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { NgFor, NgIf } from '@angular/common';
-
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
   users = DUMMY_USERS;
-  selectedUserId ?: string;
+  selectedUserId?: string;
 
-  get selectedUser(){
-    return this.users.find(user => user.id === this.selectedUserId);
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
-  onSelectUser(id: string){
+  onSelectUser(id: string) {
     this.selectedUserId = id;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, input, Output, output, computed} from '@angular/core';
+import {Component,EventEmitter,Input,input,Output,output,computed} from '@angular/core';
 
 // type User = {
 //   id: string;
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, input, Output, output, computed} from '
 //   avatar: string;
 // }
 
-interface User{
+interface User {
   id: string;
   name: string;
   avatar: string;
@@ -16,24 +16,23 @@ interface User{
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  
   // @Input({required: true}) id!: string;
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
-  @Input({required: true}) user !: User;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
-  get imagePath(){
+  get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser(){
+  onSelectUser() {
     this.select.emit(this.user.id);
   }
-  
+
   // id= input.required<string>();
   // avatar= input.required<string>();
   // name= input.required<string>();
