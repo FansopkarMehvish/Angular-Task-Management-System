3a9a06861f30f1681e54e36ca8ab76f7fdd4a6bf
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-16 12:35:08 +0530

    60. More Service Usage & Alternative Dependency Injection Mechanism

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -32,13 +32,7 @@ export class TasksComponent {
       this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData){
-    
-    this.isAddingTask = false;
-
   }
 }
