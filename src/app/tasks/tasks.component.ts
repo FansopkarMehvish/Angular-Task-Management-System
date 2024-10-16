a51e2fd2b2c840cb85eb8fa8256152df874925f4
Mehvish Fansopkar <mehvishfansopkar8@gmail.com>
2024-10-16 12:05:25 +0530

    59. Getting Started with Dependency Injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,9 +17,11 @@ export class TasksComponent {
   // @Input() name : string | undefined;
   isAddingTask: boolean = false;
 
-  
+  constructor(private tasksService: TasksService){
+  }
 
   get selectedUserTasks(){
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string){
